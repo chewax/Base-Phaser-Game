60d8b0c709427ab3c3ecbd3c2ab2d1f9a21870d5
Daniel <Daniel@dwaxmbook.local>
2015-04-23 11:12:20 -0300

    Added update function to Character Class

--- game/characters/Character.ts
+++ game/characters/Character.ts
@@ -10,5 +10,17 @@ module Characters {
         constructor (game: Phaser.Game){
             this.game = game;
         }
+
+        update () {
+            var newState = this.state.update();
+
+            // If the update returned a different state then
+            // we must exit the previous state, start the new one and assign the new one
+            if (newState !== this.state) {
+                this.state.exitState();
+                newState.enterState();
+                this.state = newState;
+            }
+        }
     }
 }
